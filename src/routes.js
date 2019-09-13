b27a6ca61f44f051de2f934e0a38d1039b45b153
Claudinei de Lima <claudinei-de-lima@hotmail.com>
2019-09-13 11:47:11 -0300

    Create routes

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login'
 import Caracteres from './pages/Caracteres'
+import CaracteresDetail from './pages/CaracteresDetail'
+import CaracteresUpdate from './pages/CaracteresUpdate'
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Route path="/" exact component={Login} />
-            <Route path="/login/" component={Login} />
-            <Route path="/caracteres/" component={Caracteres} />
-            <Route path="/caracteres/:id" component={Caracteres} />
-            <Route path="/caracteres/:id/:action" component={Caracteres} />
+            <Route path="/login/" exact component={Login} />
+            <Route path="/caracteres/" exact component={Caracteres} />
+            <Route path="/caracteres/:id" exact component={CaracteresDetail} />
+            <Route path="/caracteres/:id/:action" exact component={CaracteresUpdate} />
         </BrowserRouter>
     );
 }
